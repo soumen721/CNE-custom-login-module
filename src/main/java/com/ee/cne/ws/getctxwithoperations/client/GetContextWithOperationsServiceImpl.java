03c9b31bd7a19c837257776258cc9d97f418174e
Soumen Choudhury <sou.tcs@gmail.com>
2018-10-22 11:44:28 +0530

    update code

--- src/main/java/com/ee/cne/ws/getctxwithoperations/client/GetContextWithOperationsServiceImpl.java
+++ src/main/java/com/ee/cne/ws/getctxwithoperations/client/GetContextWithOperationsServiceImpl.java
@@ -2,14 +2,16 @@ package com.ee.cne.ws.getctxwithoperations.client;
 
 import java.net.URL;
 import javax.jws.HandlerChain;
+import org.jboss.logging.Logger;
 import com.ee.cne.ws.getctxwithoperations.generated.GetContextWithOperationsService;
 
 @HandlerChain(file = "handler-chain.xml")
 public class GetContextWithOperationsServiceImpl extends GetContextWithOperationsService {
-
+  private static final Logger log = Logger.getLogger(GetContextWithOperationsServiceImpl.class);
+  
   public GetContextWithOperationsServiceImpl(URL wsdlLocation) {
     super(wsdlLocation, SERVICE);
-    System.out.println("In GetContextWithOperationsServiceImpl");
+    log.info("In GetContextWithOperationsServiceImpl");
   }
 
 }
